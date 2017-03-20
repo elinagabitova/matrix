45e1fda34c22630ab5e8194ebe60c2f35003eb63
elinagabitova <elinagabitovaa@gmail.com>
2017-03-21 02:41:54 +0500

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,13 +1,73 @@
 #include <iostream>
 
-class matrix_t final
+#include <fstream>
+
+#include <string>
+
+#include <stdlib.h>
+
+
+
+using namespace std;
+
+
+
+class Matrix
+
 {
+
 private:
-    unsigned int rows_;
-    unsigned int columns_;
-    int** elements_;
+
+
+
+	int columns;
+
+	int strings;
+
+	int **matrix;
+
+
 public:
-    matrix_t() noexcept;
-    auto rows() -> unsigned int;
-    auto columns() -> unsigned int;
+
+
+
+	Matrix();
+
+
+	Matrix(int a, int b);
+
+
+	void print(void) const;
+
+
+	void input(char *path);
+
+	void set(int x, int y, int z);
+
+	int get(int x, int y) const;
+
+	Matrix operator+ (Matrix a) const;
+
+	Matrix operator* (Matrix a) const;
+
+
+	Matrix& operator= (Matrix &other);
+
+
+	bool operator== (Matrix &a) const;
+
+	friend istream& operator>> (istream& infile, const Matrix& result);
+
+
+	friend ostream& operator<< (ostream& os, const Matrix& a);
+
+
+
+
+	friend istream& operator>> (istream& is, Matrix& a);
+
+
+
+	~Matrix();
+
 };

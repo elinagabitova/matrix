5ebb8bfd1cd578836c651ece2b97bb18379ee274
elinagabitova <elinagabitovaa@gmail.com>
2017-03-20 23:47:32 +0500

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,86 @@
 #include <matrix.hpp>
 #include <catch.hpp>
 
-SCENARIO("matrix init", "[init]") {
-	matrix_t matrix;
-	REQUIRE(matrix.rows() == 0);
-	REQUIRE(matrix.columns() == 0);
+
+SCENARIO("par constructor") 
+
+{
+
+	Matrix matrix(2, 2);
+
+	REQUIRE(matrix.strings_() == 2);
+
+	REQUIRE(matrix.columns_() == 2);
+
 }
+
+
+
+SCENARIO("copy constructor") 
+
+{
+
+	Matrix matrix (2, 2);	
+
+	Matrix copy(matrix);	
+
+	REQUIRE(copy.strings_() == 2);
+
+	REQUIRE(copy.columns_() == 2);
+
+}
+
+
+
+
+
+SCENARIO("operator +") 
+
+{
+	Matrix matrix (2, 2);
+	Matrix matrix1 (2, 2);
+
+	std::ifstream("text.txt") >> matrix1;
+
+	REQUIRE(matrix1 + matrix == matrix + matrix );
+
+}
+
+
+
+SCENARIO("operator *") 
+
+{
+
+	Matrix matrix1 (2, 2);
+
+	Matrix matrix3 (2, 2);	
+
+	REQUIRE(matrix1 * matrix1 == matrix3);
+
+}
+
+SCENARIO("operator =") 
+
+{
+
+	Matrix matrix1(2, 2);
+
+	Matrix matrix2 = matrix1;
+
+	REQUIRE(matrix1 == matrix2);
+
+}
+
+SCENARIO("operator ==")
+
+{
+
+	Matrix matrix1(2, 2);
+
+	Matrix matrix2(2, 2);
+
+	REQUIRE(matrix1 == matrix2);
+
+}
+

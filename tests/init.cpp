99d92bbbe5c3b54cbac177e589e2d83be370735f
elinagabitova <elinagabitovaa@gmail.com>
2017-03-21 15:06:58 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,75 +1,59 @@
 #include <matrix.hpp>
 #include <catch.hpp>
 
+SCENARIO("def constructor")
+{
+	Matrix matrix;
+	REQUIRE(matrix.Strings_() == 0);
+	REQUIRE(matrix.Columns_() == 0);
+}
+
+SCENARIO("param constructor") 
+{
+	Matrix matrix(2, 2);
+	REQUIRE(matrix.Strings_() == 2);
+	REQUIRE(matrix.Columns_() == 2);
+}
 
 SCENARIO("copy constructor") 
-
 {
-
-	Matrix matrix;	
-
+	Matrix matrix (2, 2);	
 	Matrix copy(matrix);	
-
-	REQUIRE(copy.strings() == 2);
-
-	REQUIRE(copy.columns() == 2);
-
+	REQUIRE(copy.Strings_() == 2);
+	REQUIRE(copy.Columns_() == 2);
 }
 
 
 SCENARIO("operator +") 
-
 {
-	Matrix matrix (2, 2);
-
-	Matrix matrix2 (2, 2);	
-	
+	Matrix matrix1 (2, 2);
+	Matrix matrix2 (2, 2);
 	Matrix matrix3 (2, 2);	
-	std::istream("matrix.txt") >> matrix 1;
-	std::istream("matrix2.txt") >> matrix 2;
-	std::istream("sum.txt") >> matrix 3;
-	
+	std::ifstream("matrix.txt") >> matrix1;
+	std::ifstream("matrix1.txt") >> matrix2;
+	std::ifstream("sum.txt") >> matrix3;
 	REQUIRE(matrix1 + matrix2 == matrix3);
-
 }
-
-
 
 SCENARIO("operator *") 
-
 {
-
-	Matrix matrix (2,2);
-	Matrix matrix2 (2,2);
-	Matrix matrix3 (2,2);
-	std::istream("matrix.txt") >> matrix 1;
-	std::istream("matrix2.txt") >> matrix 2;
-	std::istream("multi.txt") >> matrix 3;
-
+	Matrix matrix1 (2, 2);
+	Matrix matrix2 (2, 2);
+	Matrix matrix3 (2, 2);	
+	std::ifstream("matrix.txt") >> matrix1;
+	std::ifstream("matrix1.txt") >> matrix2;
+	std::ifstream("multi.txt") >> matrix3;
 	REQUIRE(matrix1 * matrix2 == matrix3);
-
 }
-
 SCENARIO("operator =") 
-
 {
-
-	Matrix matrix (2,2);
-
-	Matrix matrix2 = matrix;
-
-	REQUIRE(matrix == matrix2);
-
+	Matrix matrix(2, 2);
+	Matrix matrix = matrix1;
+	REQUIRE(matrix == matrix1);
 }
-
 SCENARIO("operator ==")
 {
-
-	Matrix matrix (2,2);
-
-	Matrix matrix2 (2,2);
-
-	REQUIRE(matrix == matrix2);
-
+	Matrix matrix(2, 2);
+	Matrix matrix1(2, 2);
+	REQUIRE(matrix == matrix1);
 }
-
